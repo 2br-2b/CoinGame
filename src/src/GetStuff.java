5e918709f86e722b62a5bf59ae7c3faf4243cb18
2br-2b <43968991+2br-2b@users.noreply.github.com>
2019-04-29 11:09:21 -0500

    Updated GetStuff.java
    Changed numerous `.equals()` to `.startsWith()`, added more help commands, moved help commands to `getHelp()`

--- src/src/GetStuff.java
+++ src/src/GetStuff.java
@@ -13,14 +13,17 @@ import net.dv8tion.jda.core.hooks.ListenerAdapter;
 public class GetStuff extends ListenerAdapter {
 
 	private static MessageChannel c = null;
+	private static MessageReceivedEvent e = null;
 
 	@Override
-	public void onMessageReceived(MessageReceivedEvent e) {
-		if (!Main.BOTS_ALLOWED && e.getAuthor().isBot())
+	public void onMessageReceived(MessageReceivedEvent ev) {
+
+		if (!Main.BOTS_ALLOWED && ev.getAuthor().isBot())
 			return;
 
-		String message = e.getMessage().getContentRaw();
+		e = ev;
 		c = e.getChannel();
+		String message = e.getMessage().getContentRaw();
 
 		// my private channel
 		if (c.getId().equals("568466205590159361")) {
@@ -28,26 +31,18 @@ public class GetStuff extends ListenerAdapter {
 			return;
 		}
 
-		if (message.toLowerCase().contains(Main.GET_HELP_STRING)) {
-			String str = "List of commands:\n`" + Main.PREFIX + "store` shows the store.\n`" + Main.PREFIX
-					+ "buy <name>` to buy something\n`" + Main.PREFIX
-					+ "play <game_name>` to play a game.  Current games:\n\t`lotto`\n" + "\t`flip <wager>`\n"
-					+ "\t`hangman <letter>`\n`" + Main.PREFIX + "inv` shows your collection, or `" + Main.PREFIX
-					+ "bal` just shows your balance.\n`" + Main.PREFIX
-					+ "give <user_id> <item_name>` gives the user the item!\n`" + Main.PREFIX
-					+ "pay @mention <amount>` pays the other person!";
-
-			c.sendMessage(str).queue();
-		} else if (message.toLowerCase().contains(Main.GET_STORE_STRING)) {
+		if (message.toLowerCase().startsWith(Main.GET_HELP_STRING)) {
+			getHelp();
+		} else if (message.toLowerCase().startsWith(Main.GET_STORE_STRING)) {
 			// System.out.println("Show the store");
 			Store.showStore(e.getChannel());
-		} else if (message.equals(Main.GET_MONEY_STRING)) {
+		} else if (message.startsWith(Main.GET_MONEY_STRING)) {
 			String str = e.getAuthor().getName() + " has " + Main.addCommas(Main.bal.get(e.getAuthor().getId()))
 					+ Main.CURRENCY;
 			// System.out.println(str);
 			c.sendMessage(str).queue();
 
-		} else if (message.equals(Main.GET_INVENTORY_STRING)) {
+		} else if (message.startsWith(Main.GET_INVENTORY_STRING)) {
 			String str = e.getAuthor().getName() + "'s inventory:\n"
 					+ Main.addCommas(Main.bal.get(e.getAuthor().getId())) + Main.CURRENCY;
 
@@ -57,7 +52,7 @@ public class GetStuff extends ListenerAdapter {
 				ArrayList<Upgrade> list = Main.upgrades.get(e.getAuthor().getId());
 				for (Upgrade u : list) {
 					boost += u.getTotalBoost();
-					str += "\n`" + u.getQuantity() + "x` " + u.getName();
+					str += "\n`" + u.getQuantity() + "x` " + u.getNamePrefix();
 				}
 			}
 
@@ -70,6 +65,43 @@ public class GetStuff extends ListenerAdapter {
 
 		serializeStuff();
 
+	}
+
+	private void getHelp() {
+		String str = "";
+		String[] list = e.getMessage().getContentRaw().split(" ");
+		if (list.length > 1) {
+			switch (list[1]) {
+			case "prefix":
+				str = Store.outdatedPrefix + " means that something is not sold in the store anymore.\n"
+						+ "\uu221E is from the Avengers: Endgame event.\n" + MergableHandler.prefix
+						+ " means that the upgrade was merged from two other upgrades.";
+				break;
+
+			case "merge":
+				for (Mergable m : MergableHandler.possibleMerges) {
+					str += m + "\n";
+				}
+
+				break;
+
+			default:
+				System.err.println("getHelp Error");
+				break;
+			}
+		} else {
+			str = "List of commands:\n`" + Main.PREFIX + "store` shows the store.\n`" + Main.PREFIX
+					+ "buy <name>` to buy something\n`" + Main.PREFIX
+					+ "play <game_name>` to play a game.  Current games:\n\t`lotto`\n" + "\t`flip <wager>`\n"
+					+ "\t`hangman <letter>`\n`" + Main.PREFIX + "inv` shows your collection, or `" + Main.PREFIX
+					+ "bal` just shows your balance.\n`" + Main.PREFIX
+					+ "give <user_id> <item_name>` gives the user the item!\n`" + Main.PREFIX
+					+ "pay @mention <amount>` pays the other person\n`" + Main.PREFIX
+					+ "help prefix` lists the item prefixes and what they mean\n`" + Main.PREFIX
+					+ "help merge` lists the possible merges";
+
+		}
+		c.sendMessage(str).queue();
 	}
 
 	private void serializeStuff() {
