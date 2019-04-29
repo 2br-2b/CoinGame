0cbd2f763b7733987b2392de6e3146bfadfbab1b
2br-2b <43968991+2br-2b@users.noreply.github.com>
2019-04-29 11:11:05 -0500

    Changed UpdateMerge to MergeHandler

--- src/src/Main.java
+++ src/src/Main.java
@@ -38,7 +38,7 @@ public class Main {
 		jda.addEventListener(new GetStuff());
 		jda.addEventListener(new Store());
 		jda.addEventListener(new PlayGames());
-		jda.addEventListener(new UpgradeMerge());
+		jda.addEventListener(new MergableHandler());
 
 		new FileManager();
 		serializeStuff();
