2449640a222bbb91d776fcc253ec3a83cad142ae
2br-2b <43968991+2br-2b@users.noreply.github.com>
2019-05-04 16:46:06 -0500

    Now, you can list other peoples' scars.

--- src/src/Command_Get_Scars.java
+++ src/src/Command_Get_Scars.java
@@ -3,6 +3,8 @@ package src;
 import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
 
+import net.dv8tion.jda.core.EmbedBuilder;
+
 public class Command_Get_Scars extends Command {
 
 	public Command_Get_Scars() {
@@ -15,8 +17,37 @@ public class Command_Get_Scars extends Command {
 
 	@Override
 	protected void execute(CommandEvent event) {
+		String id;
+		try {
+			id = event.getArgs().replace("!", "").trim().substring(2, 20);
+		} catch (StringIndexOutOfBoundsException e) {
+			id = event.getAuthor().getId();
+		}
 
-		event.reply(ScarHandler.getScars(event.getAuthor().getId()).toString());
+		String str = "";
+
+		int boost = 0;
+		if (ScarHandler.scars.containsKey(id)) {
+
+			for (Scar s : ScarHandler.getScars(id)) {
+				boost += s.getTotalDamage();
+				str += "\n`" + s.getQuantity() + "x` " + s.getName() + ": `" + Main.addCommas(s.getBoost()) + "`"
+						+ Main.CURRENCY;
+			}
+
+		} else {
+			event.reply("<@" + id + "> doesn't have any scars… yet.");
+			return;
+		}
+
+		str += "\nTotal loss: " + Main.addCommas(boost) + Main.CURRENCY;
+
+		EmbedBuilder emb = new EmbedBuilder();
+
+		emb.setTitle(Main.getUserFromID(id).getName() + "'s Scars");
+		emb.setDescription(str);
+
+		event.reply(emb.build());
 	}
 
 }
